3adfca13a6f53cea612bb60364d385adcfbdb118
nabilaadama <nabilaa.adamaa@gmail.com>
2024-12-07 18:54:54 +0800

    update:sidebar admin

--- src/components/admin-com/sidebar.tsx
+++ src/components/admin-com/sidebar.tsx
@@ -2,6 +2,9 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+
 
 const navLinks = [
   { name: "Home", href: "/admin" },
@@ -10,15 +13,17 @@ const navLinks = [
   { name: "History", href: "/admin/data-pengiriman" },
 ];
 
+
 export function Sidebar3() {
+  const pathname = usePathname(); 
 
 
   return (
-    <div className="w-60 h-screen bg-white p-5 fixed top-0 left-0 shadow-sm border-r-[#0A4635]/50 border-r-2">
+    <div className="w-60 h-screen bg-[#0A4635] fixed top-0 left-0 shadow-md ">
       {/* Logo Section */}
-      <div className="items-center justify-center"> {/* Tambahkan margin-bottom untuk memberi jarak */}
+      <div className="items-center justify-center pl-10 py-6 pr-5 "> 
         <Image
-          src="/assets/logo-hijau.png"
+          src="/assets/logo-cream.png"
           alt="Cloohloop Logo"
           width={150}
           height={50}
@@ -26,24 +31,31 @@ export function Sidebar3() {
       </div>
 
       {/* Menu Section */}
-      <ul className="space-y-4 mt-10">
+      <ul className=" space-y-1 mt-5">
         {navLinks.map((item, index) => (
           <li key={index}>
             <Link
               href={item.href}
-              className="flex items-center text-md font-medium text-[#0A4635] hover:bg-[#E8F5E9] p-2 rounded-md "
+              className={`flex items-center text-md font-medium text-[#FAF7F1] border-[#0A4635] hover:bg-[#E8F5E9]/30 gap-4 px-4 py-5  border-l-4 hover:border-[#FFEA7F] hover:text-[#FFEA7F]"
+              ${
+                pathname === item.href
+                  ? "bg-[#E8F5E9]/30 text-[#FFEA7F] border-l-4 border-[#FFEA7F]" // Aktif
+                  : "text-[#FAF7F1] hover:bg-[#E8F5E9]/30 hover:text-[#FFEA7F] hover:border-l-4 hover:border-[#FFEA7F]"
+              }`}
             >
-              <span className="mr-3">{item.name}</span>
+            <span className="pl-3">{item.name}</span>
             </Link>
           </li>
         ))}
       </ul>
 
-      <div className="border-t-2 border-[#0A4635]/50 mt-3">
-        <li className="mt-3 flex items-center text-md font-medium text-[#0A4635] hover:bg-[#E8F5E9] p-2 rounded-md cursor-pointer"
-          >
-          Logout {/* blmpa bisa kasih logout jadi button nya mo saja dlu diksih muncul alias ndbisa dipencet :'( */}
-        </li>
+      <div className="border-t border-[#f4ede6]/30 mt-5">
+        <div className="text-[#f4ede6] border-[#0A4635] hover:bg-[#E8F5E9]/30 gap-4 px-4 py-5  mt-3 border-l-4 hover:border-[#FFEA7F] hover:text-[#FFEA7F]">
+          <li className="flex items-center text-md font-medium pl-3"
+            >
+            Logout {/* blmpa bisa kasih logout jadi button nya mo saja dlu diksih muncul alias ndbisa dipencet :'( */}
+          </li>
+        </div>
       </div>
     </div>
   );
