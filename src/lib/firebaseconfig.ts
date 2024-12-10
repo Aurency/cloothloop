256995050e5e6a3b67968a1cdc2cff3d49ecc660
nabilaadama <nabilaa.adamaa@gmail.com>
2024-12-10 22:53:13 +0800

    update:firebase config

--- src/lib/firebaseconfig.ts
+++ src/lib/firebaseconfig.ts
@@ -7,14 +7,13 @@ import { getStorage } from "firebase/storage"; // Firebase Storage
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlXqSsuRGjce-9v4USNghyv6dTnTmE45o",
-  authDomain: "clothloop-dfbb2.firebaseapp.com",
-  databaseURL: "https://clothloop-dfbb2-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "clothloop-dfbb2",
-  storageBucket: "clothloop-dfbb2.firebasestorage.app",
-  messagingSenderId: "117199771212",
-  appId: "1:117199771212:web:9183edc345835e4f10c6e9",
-  measurementId: "G-M5S3K35ZQ4"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Inisialisasi Firebase
